61c3bdcaefd87f7fc200bbc3dc51723081f30a2c
Pedro Paulo Skakum <pedroskakum@gmail.com>
2016-06-16 16:49:10 -0300

    Removendo implementação de Autenticable desnecessária no model usuario

--- app/Models/Usuario.php
+++ app/Models/Usuario.php
@@ -3,13 +3,9 @@
 namespace Segundo\Models;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Auth\Authenticatable;
-use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
 
-class Usuario extends Model implements AuthenticatableContract
+class Usuario extends Model
 {
-    use Authenticatable;
-
     public $timestamps = false;
     
     /**
@@ -29,65 +25,4 @@ class Usuario extends Model implements AuthenticatableContract
     protected $hidden = [
         'password', 'remember_token',
     ];
-
-    /**
-     * Get the name of the unique identifier for the user.
-     *
-     * @return string
-     */
-    public function getAuthIdentifierName()
-    {
-        // TODO: Implement getAuthIdentifierName() method.
-    }
-
-    /**
-     * Get the unique identifier for the user.
-     *
-     * @return mixed
-     */
-    public function getAuthIdentifier()
-    {
-        // TODO: Implement getAuthIdentifier() method.
-    }
-
-    /**
-     * Get the password for the user.
-     *
-     * @return string
-     */
-    public function getAuthPassword()
-    {
-        // TODO: Implement getAuthPassword() method.
-    }
-
-    /**
-     * Get the token value for the "remember me" session.
-     *
-     * @return string
-     */
-    public function getRememberToken()
-    {
-        // TODO: Implement getRememberToken() method.
-    }
-
-    /**
-     * Set the token value for the "remember me" session.
-     *
-     * @param  string $value
-     * @return void
-     */
-    public function setRememberToken($value)
-    {
-        // TODO: Implement setRememberToken() method.
-    }
-
-    /**
-     * Get the column name for the "remember me" token.
-     *
-     * @return string
-     */
-    public function getRememberTokenName()
-    {
-        // TODO: Implement getRememberTokenName() method.
-    }
 }
